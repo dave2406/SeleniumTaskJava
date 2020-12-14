4fb6d5125201d09eaf630dcc3308ae6d05ae7698
dave2406 <dave2406@hotmail.co.uk>
2020-12-14 19:27:52 +0000

    Create project, set up selenium and base classes

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest{
+        protected static WebDriver driver;
+
+        @BeforeClass
+        public static void setUp(){
+            System.setProperty("webdriver.chrome.driver", "src/test/java/drivers/chromedriver.exe");
+            driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        }
+
+        @After
+        public void cleanUp(){
+            driver.manage().deleteAllCookies();
+        }
+
+        @AfterClass
+        public static void tearDown(){
+            driver.close();
+        }
+}
